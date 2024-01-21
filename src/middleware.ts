ad1fafc02b9f64df86a6fbc361480a1ae0fa78c8
Nandkishor Kumawat <nandkishor41071@gmail.com>
2024-01-21 16:53:03 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export default function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
   const isPublicPath = path === "/login" || path === "/signup";
