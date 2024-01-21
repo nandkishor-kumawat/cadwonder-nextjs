2b6367188e0b0c71b46f508fcc52aa7f1b369e79
Nandkishor Kumawat <nandkishor41071@gmail.com>
2024-01-21 17:02:29 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,4 @@
-// export { default } from 'next-auth/middleware'
-
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export default function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
-
-  const isPublicPath = path === "/login" || path === "/signup";
-
-  const token = request.cookies.get("next-auth.session-token")?.value || "";
-  const callback = request.cookies.get("next-auth.callback-url")?.value || "";
-  console.log(request.cookies)
-
-  if (isPublicPath && token) {
-    return NextResponse.redirect(new URL('/', request.nextUrl));
-  }
-
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL("/login?callbackUrl="+callback, request.nextUrl));
-  }
-}
-
+export { default } from 'next-auth/middleware'
 
 
 
